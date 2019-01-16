4a891e8d084383cc887c42f8f479f371e890efe7
Roger Garcia <r1ger_pg@hotmail.com>
2019-01-16 12:22:05 +0530

    Add boilerplate private routing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,123 @@
-import React from 'react'
+
 import { render } from 'react-dom'
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Redirect,
+	withRouter
+} from "react-router-dom";
+
 import './stylesheets/ui.scss'
-import { App } from './components/App'
-import { Whoops404 } from './components/Whoops404'
-import { LoginPage } from './components/Login'
-import { Router, Route, hashHistory } from 'react-router'
+
+
+function AuthExample() {
+  return (
+    <Router>
+      <div>
+        <AuthButton />
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/public" component={Public} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/protected" component={Protected} />
+      </div>
+    </Router>
+  );
+}
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    fakeAuth.isAuthenticated ? (
+      <p>
+        Welcome!{" "}
+        <button
+          onClick={() => {
+            fakeAuth.signout(() => history.push("/"));
+          }}
+        >
+          Sign out
+        </button>
+      </p>
+    ) : (
+      <p>You are not logged in.</p>
+    )
+);
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function Public() {
+  return <h3>Public</h3>;
+}
+
+function Protected() {
+  return <h3>Protected</h3>;
+}
+
+class Login extends React.Component {
+  state = { redirectToReferrer: false };
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  render() {
+    let { from } = this.props.location.state || { from: { pathname: "/" } };
+    let { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) return <Redirect to={from} />;
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
 
 window.React = React
 
 render(
-	<Router history={hashHistory} >
-		<Route path="/" component={App} />
-		<Route path="/login" component={LoginPage} />
-		<Route path="list-days" component={App}>
-			<Route path=":filter" component={App} />
-		</Route>
-		<Route path="add-day" component={App} />
-		<Route path="members" component={App} />
-		<Route path="*" component={Whoops404} />
-	</Router>,
+	<AuthExample/>,
 	document.getElementById('react-container')
 )
