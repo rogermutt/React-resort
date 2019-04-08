1bd4fb98cd72b985a90d32d6e33be39b51c23a04
Roger Garcia <r1ger_pg@hotmail.com>
2019-04-08 13:53:00 +0530

    Registration API calls

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,6 +9,24 @@ const HTTP_Request = (url, type, headers = null, body) => {
 	   })  
 }
 
+const LOGIN_DETAILS = {
+  "email": 'r@r.com', 
+  "password": '123456'
+}
+
+function login (loginParams) {  
+  return fetch(URL_LOGIN, {
+      method: "POST",
+      headers: {
+      "Accept":"application/json",
+      "Content-Type":"application/json"
+      },
+      body: JSON.stringify(loginParams)
+  })
+    .then(res => res.json())     
+    .catch(error => console.log(error))			
+}
+
 const URL_SIGNUP = 'http://localhost:3001/api/v1/users'
 const URL_LOGIN = 'http://localhost:3001/authenticate'
 const RESORT_URL = 'http://localhost:3001/api/v1/resorts'
@@ -52,7 +70,10 @@ export class SignUpForm extends Component {
       return HTTP_Request(URL_SIGNUP, "POST", logIn_Headers, JSON.stringify(newUser))
         .then(res => res.json())
         .then(res => {
-  
+
+          login(LOGIN_DETAILS)
+          .then(res => {
+
             if (res.error) {
               for (var message in res.error) {
                 console.log('res ' + res.error[message]);
@@ -60,9 +81,9 @@ export class SignUpForm extends Component {
               }
             }
   
-            if (res.auth_token) {   
+            if (res.token) {    
   
-            localStorage.setItem('token', res.auth_token) 
+            localStorage.setItem('token', res.token) 
             fetch(RESORT_URL, {
                 headers: {
                   'Authorization': localStorage.getItem('token')
@@ -71,14 +92,22 @@ export class SignUpForm extends Component {
               .then(res => res.json())					
               .then (allSkiDays => {
 
-                this.props.auth(true, allSkiDays)
+                console.log('allSkiDays ', allSkiDays);
+                
+
+                // this.props.auth(true, allSkiDays)
                 this.props.history.push("/dayList")
     
-              })		
-              }
+              })
+              .catch(error => console.log(error))  		
+              }            
+
+          })
+
+  
         
           })      
-          .catch(error => console.log(error))  
+ 
                      
     }    
   
